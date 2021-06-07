cedd028439f1f1d88bfd42ee5e7edcd01d476dc7
vladimir <nighshade2903@gmail.com>
2021-06-07 21:02:32 +0300

    attemps value

--- src/test/java/loginFeature/service/LoginServiceTest.java
+++ src/test/java/loginFeature/service/LoginServiceTest.java
@@ -10,15 +10,16 @@ import org.junit.Test;
 public class LoginServiceTest {
 
     LoginService loginService;
+    User user;
 
     @Before
     public  void setUp(){
        this.loginService = new LoginService();
+        this.user = getUser();
+        Assert.assertEquals(3,user.getLoginAttempts());
     }
     @Test
     public void checkUserPassword_positive() {
-        User user = getUser();
-
         String userInput = "password";
 
 
@@ -31,7 +32,6 @@ public class LoginServiceTest {
 
     @Test
     public void checkUserPassword_negative() {
-        User user = getUser();
 
         String userInput = "wrong";
 
