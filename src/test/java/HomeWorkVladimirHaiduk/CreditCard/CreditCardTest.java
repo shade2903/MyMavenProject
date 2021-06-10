64dcf3892c1aa866ee049c090dce3fe86b0fa072
shade2903 <nighshade2903@gmail.com>
2021-06-10 08:07:38 +0300

    Credit card test

--- src/test/java/HomeWorkVladimirHaiduk/CreditCard/CreditCardTest.java
+++ src/test/java/HomeWorkVladimirHaiduk/CreditCard/CreditCardTest.java
@@ -4,6 +4,8 @@ import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import static org.junit.Assert.assertEquals;
+
 public class CreditCardTest {
     Logger log = LoggerFactory.getLogger("CreditCardTest");
     CreditCard card = new CreditCard("test", 1234);
@@ -16,27 +18,20 @@ public class CreditCardTest {
         card.deposit(1234, 500);
         card.deposit(1234, 1500);
         double actual = card.getBalance();
-        if (actual == expected) {
-            log.info("Test success");
-        } else {
-            log.info("Test filed");
-        }
+
+
+        assertEquals(expected,actual,0);
+
     }
 
     @Test
     public void withdraw() {
         double expected = 800;
         card.setLimit(1500);
-
         card.deposit(1234,500);
         card.withdraw(1234,1300);
-
         double actual = card.getLoanDebt();
-        if(actual==expected){
-            log.info("Test success");
-        }else{
-            log.info("Test filed");
-        }
+        assertEquals(expected,actual,0);
 
 
     }
